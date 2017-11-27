c7b7cdc9af20d2fc58b8580c07bf406513d78723
Krishna Regmi <kisna72@gmail.com>
2017-11-26 18:14:32 -0600

    add bootstrap slider in the homepage

---
+++ umami.js
@@ -0,0 +1,22 @@
+$(window).scroll(function() {
+  var showing_shrunk = false;
+
+  if ($(document).scrollTop() > 50) {
+    //$('nav').addClass('shrink');
+    if (!showing_shrunk){
+	    console.log("add class to shrink the thing.");
+	    $("#shrunk_menu").removeClass("invisible");
+	    $("#shrunk_menu").addClass("visible");
+	    $("#full_menu").removeClass("visible");
+	    $("#full_menu").addClass("invisible");
+	    showing_shrunk = true;
+	}
+  } else {
+    console.log("add class to expand the menu.");
+    $("#shrunk_menu").removeClass("visible");
+    $("#shrunk_menu").addClass("invisible");
+    $("#full_menu").removeClass("invisible");
+    $("#full_menu").addClass("visible");
+    showing_shrunk = false;
+  }
+});
